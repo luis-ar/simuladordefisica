bbe6cdd4d6eb88e9b7ee097dcd3cc340788ce7c3
Luis <luisdemaryori@gmail.com>
2022-04-12 17:14:19 -0500

    moon

--- javas.js
+++ javas.js
@@ -1,6 +1,27 @@
+let voltaje = document.querySelector(".voltaje");
+let resistencia = document.querySelector(".resistencia");
 let boton = document.querySelector(".boton");
-let contenedor = document.querySelector(".contenedor");
-boton.addEventListener("click", function (e) {
-  alert("jaja");
-  contenedor.style.background = "red";
-});
+var pantalla = document.querySelector(".pantalla");
+let datos = document.querySelector(".datos");
+
+function inicio() {
+  let num1;
+  let num2 = 4;
+  num1 = voltaje.textContent;
+  num2 = resistencia.textContent;
+  boton.onclick = function (e) {
+    respuesta();
+  };
+  datos.onclick = function (e) {
+    limpiar();
+  };
+  function respuesta() {
+    resultado = num1 * num2;
+    pantalla.textContent = "Corriente=" + resultado;
+  }
+  function limpiar() {
+    voltaje.textContent = "";
+    num1 = 0;
+    num2 = 0;
+  }
+}
